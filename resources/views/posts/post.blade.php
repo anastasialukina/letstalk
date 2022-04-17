df6c6fb70224d1a14c6b25046aae8439919b8e3f
Anastasia <anastasia.lukina1602@icloud.com>
2022-04-18 01:41:59 +0300

    Add anchor

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,8 +1,9 @@
 <div class="card" style="margin-bottom: 8px">
-    <div class="card-body">
+    <div class="card-body" id="{{ "post_$post->id" }}">
         <blockquote class="blockquote mb-0">
             <p>{{ $post->text }}</p>
             <footer class="blockquote-footer">
+                <a href="#{{ "post_$post->id" }}">#{{ $post->id }}</a>
                 <cite title="Source Title">{{ $post->author->name }}</cite>
             </footer>
         </blockquote>
